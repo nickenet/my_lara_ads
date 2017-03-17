a69e8cd646d41d94cec0b74672b68fdc627e7ea8
nickenet <nicke.ipxp@gmail.com>
2017-03-17 22:21:08 +0200

    yandex-metrika

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -100,7 +100,11 @@
 </head>
 <body>
     <h1></h1>
-    
+  <!--counters -->
+    <!-- Yandex.Metrika counter --> 
+	<script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter42723354 = new Ya.Metrika({ id:42723354, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/42723354" style="position:absolute; left:-9999px;" alt="" /></div></noscript> 
+    <!-- /Yandex.Metrika counter -->
+<!--end -->  
 <div id="wrapper">
 
 	@section('header')
@@ -152,6 +156,7 @@
 @show
 
 @section('javascript')
+
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"> </script> -->
 <script src="{{ url('/assets/js/jquery/1.10.1/jquery-1.10.1.js') }}"></script>
 <script src="{{ url('/assets/bootstrap/js/bootstrap.min.js') }}"></script>
@@ -200,10 +205,6 @@
 
 {!! config('settings.seo_google_analytics') !!}
 
-<!--counters -->
-    <!-- Yandex.Metrika counter --> 
-	<script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter42723354 = new Ya.Metrika({ id:42723354, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/42723354" style="position:absolute; left:-9999px;" alt="" /></div></noscript> 
-    <!-- /Yandex.Metrika counter -->
-<!--end -->
+
 </body>
 </html>
